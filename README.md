f2e7d176bd2c1ee84c87cb7b2db0f41bea37c167
cmpwr <compiledwrong@gmail.com>
2017-04-07 21:39:55 +0530

    some todo items

---
+++ README.md
@@ -0,0 +1,4 @@
+TODO:
+- dry run mode, for no sessions in parallel
+- generate Session signups doc
+- combined, separate, specialized, dojos: separate, by client team
--- src/java/com/cmpwrn/calentwu/CalendarGenerator.java
+++ src/java/com/cmpwrn/calentwu/CalendarGenerator.java
@@ -4,10 +4,12 @@ public class CalendarGenerator {
     private String config;
 
     public CalendarGenerator(String config) {
+
         this.config = config;
     }
 
     public String generate(CalendarEvent... calendarEvents) {
+//        TODO
         return config;
     }
 }
